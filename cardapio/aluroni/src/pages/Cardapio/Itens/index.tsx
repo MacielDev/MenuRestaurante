dc465d15099da1969a2eabaf29f0003c49109c2d
MacielDev <andreraizdetudo@gmail.com>
2022-09-01 19:08:35 -0300

    Finalizando listagem dos pratos

---
+++ cardapio/aluroni/src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,19 @@
+import itens from './itens.json';
+import Item from './Item';
+import styles from './Itens.module.scss';
+
+export default function Itens(){
+    return (
+        <div className={styles.itens}>
+            {
+                itens.map(item => (
+                        <div>
+                            <Item 
+                                key={item.id}
+                                {...item}
+                            />
+                        </div>
+                ))}
+        </div>
+    )
+}
--- cardapio/aluroni/src/pages/Cardapio/index.tsx
+++ cardapio/aluroni/src/pages/Cardapio/index.tsx
@@ -1,14 +1,17 @@
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 import Buscador from './Buscador';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
-
+import Itens from './Itens';
 
 export default function Cardapio(){
+
     const [busca,setBusca] = useState("");
     const [filtro,setFiltro]  = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
+
     return (
         <main>
             <nav className={styles.menu}>
@@ -28,8 +31,9 @@ export default function Cardapio(){
                     setBusca={setBusca}/>
                 <div className={styles.cardapio__filtros}>
                     <Filtros filtro = {filtro} setFiltro={setFiltro}/>
-                    <Ordenador />
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
                 </div>
+                <Itens />
             </section>
         </main>
     );
