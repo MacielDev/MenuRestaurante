9aeb8d3d720173603acce134c7962b85e9bb48de
MacielDev <andreraizdetudo@gmail.com>
2022-09-06 00:19:08 -0300

    Utilizando estados via useNavigate e useLocation

--- cardapio/aluroni/src/pages/Inicio/index.tsx
+++ cardapio/aluroni/src/pages/Inicio/index.tsx
@@ -1,16 +1,22 @@
 import cardapio from 'data/cardapio.json';
 import styles from  './Inicio.module.scss';
-
+import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Inicio(){
+  const navigate = useNavigate();
   let pratosRecomendados = [...cardapio];
   pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
-     
+  
+  function redirecionarParaDetalhes(prato:typeof cardapio[0]){
+    navigate(`/prato/${prato.id}`,{state:{ prato },replace:true});
+  }
   return (
     <section>
       
-      <h3 className={styles.titulo}>
+      <h3 className={stylesTema.titulo}>
         Recomendações da cozinha
       </h3>
       <div className={styles.recomendados}>
@@ -19,11 +25,21 @@ export default function Inicio(){
             <div className={styles.recomendado__imagem}>
               <img src={item.photo} alt={item.title} />
             </div>
-            <button className={styles.recomendado__botao}>
+            <button 
+              className={styles.recomendado__botao}
+              onClick={() => redirecionarParaDetalhes(item) }
+            >
               Ver mais
             </button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa Casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do Aluroni"/>
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3185 <br/><br/> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
--- cardapio/aluroni/src/routes.jsx
+++ cardapio/aluroni/src/routes.jsx
@@ -3,21 +3,30 @@ import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre ';
+import Footer from 'pages/Footer';
+import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
+
 
 
 export default function AppRouter(){
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu/>
         
         <Routes>
           <Route path="/" element={<PaginaPadrao />}>
             <Route index element={<Inicio />}/>
-            <Route path="cardapio" element={<Cardapio />}/>
+            <Route path='cardapio' element={<Cardapio />}/>
+            <Route path='cardapio' element={<Cardapio/>} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
-          <Route path="/cardapio" element={<Cardapio/>} />
+          <Route path='*' element={<NotFound/>}/>
+          <Route path='prato/:id' element={<Prato/>}/>
         </Routes>
+        <Footer/>
       </Router>
     </main>
   );
